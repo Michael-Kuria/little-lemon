e18eac86eab126a4836b75b4fb077e28d5ac17b0
micleah <mchege78@gmail.com>
2023-02-04 04:46:52 +0300

    added styling for the menu section

--- src/components/MenuSection.jsx
+++ src/components/MenuSection.jsx
@@ -3,20 +3,17 @@ import React from "react";
 const Menu = ({ image, foodName, foodDescription, price }) => {
   return (
     <div className="menu-container">
-      <div>
-        <img
-          src={image}
-          width=""
-          height="500px"
-          alt="this is a picture of food"
-        />
+      <div className="menu-image">
+        <img src={image} width="" height="" alt="this is a picture of food" />
       </div>
-      <div className="food-description">
-        <p>{foodName}</p>
-        <p>${price}</p>
-      </div>
-      <div>
-        <p>{foodDescription}</p>
+      <div className="food-description-container">
+        <div className="food-price-container">
+          <p className="food-title">{foodName}</p>
+          <p style={{ color: "red" }}>${price}</p>
+        </div>
+        <div>
+          <p>{foodDescription}</p>
+        </div>
       </div>
     </div>
   );
@@ -51,14 +48,23 @@ const food = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
     price: "2000",
   },
+  {
+    image: "/assets/bruchetta.svg",
+    foodName: "Bruchetta",
+    foodDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    price: "2000",
+  },
 ];
 export default function MenuSection() {
   return (
-    <section>
+    <section className="menu-section">
       <div className="menu-section-container">
         <div className="menu-section-header">
           <p className="title">This Week's Special</p>
-          <button>Order Online</button>
+          <div>
+            <button>Order Online</button>
+          </div>
         </div>
         <div className="menu-section-scroller">
           {food.map((item) => (
